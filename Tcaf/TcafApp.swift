21726c5ea60a857bf3fa12a91782d0c32ee0f66c
Vitalii Kravets <vitalii.kravetss@gmail.com>
2022-12-18 23:14:14 +0200

    Compose the app inside the Main module – `TcafApp`

--- Tcaf/TcafApp.swift
+++ Tcaf/TcafApp.swift
@@ -8,10 +8,28 @@ import SwiftUI
 @main
 struct TcafApp: App {
     @State var store = ContentViewStore()
+    let baseURL = URL(string: "http://numbersapi.com")!
+    let numberFactLoader = makeNumberFactLoader()
     
     var body: some Scene {
         WindowGroup {
             ContentView(store: $store)
+                .onAppear {
+                    store.numberFactAction = { getFact(pathComponent: store.text) }
+                    store.randomNumberFactAction = { getFact(pathComponent: "random/math") }
+                }
         }
     }
+    
+    func getFact(pathComponent: String) {
+        Task {
+            store.numberFact = try await numberFactLoader.getNumberFact(from: baseURL.appendingPathComponent(pathComponent))
+        }
+    }
+}
+
+private func makeNumberFactLoader() -> NumberFactLoader {
+    let session = URLSession(configuration: .default)
+    let client = URLSessionHTTPClient(session: session)
+    return RemoteNumberFactLoader(client: client)
 }
