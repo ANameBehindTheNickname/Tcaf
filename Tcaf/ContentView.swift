2afada30f735c11b6e0ce9010e0250e77e0cbf67
Vitalii Kravets <vitalii.kravetss@gmail.com>
2022-12-18 22:46:31 +0200

    Initial Commit

---
+++ Tcaf/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//
+//  Copyright © 2022 ANameBehindTheNickname. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tcaf/TcafApp.swift
@@ -0,0 +1,15 @@
+//
+//
+//  Copyright © 2022 ANameBehindTheNickname. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct TcafApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
